02b62993eaba679fd50686ff2f519bc8a3b36dad
Work <pumba@meta.ua>
2019-09-26 22:35:06 +0300

    js mod

--- app/static/js/grid.js
+++ app/static/js/grid.js
@@ -1,8 +1,25 @@
 var canvas = document.getElementById("play");
 const c = canvas.getContext('2d')
-for (let i = 0; i < 30; i++) {
-  for (let j = 0; j < 30; j++) {
-    /*c.strokeStyle = `rgb(${i * 5}, ${j * 5}, ${(i+j) * 50})`*/
+elem.addEventListener('click', event_click);
+
+function draw() {
+event_click();
+    for (let i = 0; i < 30; i++) {
+    for (let j = 0; j < 30; j++) {
+    c.strokeStyle = `rgb(192, 192, 192)`
     c.strokeRect(j * 40, i * 40, 40, 40)
-  }
+        }
+    }
 }
+requestAnimationFrame(draw);
+
+function event_click() {
+console.log (pageX);
+console.log (pageY);
+}
+
+/*function ball()  {
+
+}*/
+
+
