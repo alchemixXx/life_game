7fe799a91f818138baf78905ec4a2fb251d6c0d7
Work <pumba@meta.ua>
2019-09-26 21:27:16 +0300

    1

--- app/static/js/grid.js
+++ app/static/js/grid.js
@@ -1,13 +1,8 @@
-var grid = document.getElementById("grid");
-var bg =      new   Image();
-var context = grid.getContext('2d');
-document.addEventListener('mousemove', playerMove);
-bg.src = "app/static/images/Simple-Blue-Background.jpg";
-
-    // отрисовка сетки
-    function draw() {
-
-    context.drawImage(bg, 0, 0); // прорисовка бэкграунда
-
-    }
-    requestAnimationFrame(draw);
+var canvas = document.getElementById("play");
+const c = canvas.getContext('2d')
+for (let i = 0; i < 30; i++) {
+  for (let j = 0; j < 30; j++) {
+    /*c.strokeStyle = `rgb(${i * 5}, ${j * 5}, ${(i+j) * 50})`*/
+    c.strokeRect(j * 40, i * 40, 40, 40)
+  }
+}
