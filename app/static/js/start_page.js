6e1fce74178c2de9d9031adf778d67709a1d97b1
Work <pumba@meta.ua>
2019-09-29 20:21:02 +0300

    start page new

--- app/static/js/start_page.js
+++ app/static/js/start_page.js
@@ -13,8 +13,8 @@ ctx.lineWidth = 5; ctx.lineJoin = "round"; ctx.globalAlpha = 2/3;
 function draw() {
 text_animate(220, 220, 100, 'Игра "ЖИЗНЬ"', 250, 150, 0, "black");
 text_animate(220, 220, 100, 'Выбери сетку игры"', 180, 230, 0, "green");
-text_animate(220, 220, 50, '12х8  15х10  8х10  18х12  30х20', 20, 330, 0, "blue");
-text_animate(220, 220, 50, '36х24  45x30  60x40  90x60  ', 60, 430, 0, "blue");
+text_animate(220, 220, 50, '12х8 15х10 18х12 30х20 36х24', 40, 330, 0, "blue");
+text_animate(220, 220, 50, '45x30  60x40  90x60  180x120 ', 40, 430, 0, "blue");
 }
 
 function text_animate (dashLen, dashOffset, speed, txt, x, y, i, color) {
