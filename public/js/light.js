156197f0ddba4cbc02e65ecb9c6e3824012ee0de
Work <pumba@meta.ua>
2020-09-24 22:33:41 +0300

    for backup

--- public/js/light.js
+++ public/js/light.js
@@ -1,33 +1,27 @@
 
 
-function light (position.x,) {
-    class light {
-        constructor(position.x,position.y,position.z, color, intensity, distance ) {
-            this.position.x = position.x;
-            this.position.y = position.y;
-            this.position.z = position.z;
-            this.color = color;
-            this.intensity = intensity;
-            this.distance = distance;
-        }
 
-    }
-
+function lightPoint (name, position.x,position.y,position.z, color, intensity, distance) {
+let light = new THREE.PointLight(color, intensity, distance);
+light.position.set(position.x, position.y, position.z);
+light.name = name;
+scene.add(light);
 }
+let lightPoint1 = lightPoint( 'light1',800, -350, 450, 0xffffff, 1.0, 1000)
 
-
-
-let light = new THREE.PointLight(0xffffff, 1.0, 1000);
-  let light1 = new THREE.PointLight(0xffffff, 2.0, 1000);
-  let light2 = new THREE.PointLight(0xffffff, 2.0, 1000);
-  let light3 = new THREE.PointLight(0xffffff, 2.0, 1000);
-  let light4 = new THREE.PointLight(0xffffff, 1.0, 1000);
-  // устанавливаем координаты света
-  light.position.set(800, -350, 450);
-  light1.position.set(-100, -100, 350);
-  light2.position.set(200, -750, -50);
-  light3.position.set(1100, -450, -50);
-  light4.position.set(1000, -50, 650);
+// let light = function lights() {
+//     let light = new THREE.PointLight(0xffffff, 1.0, 1000);
+//     let light1 = new THREE.PointLight(0xffffff, 2.0, 1000);
+//     let light2 = new THREE.PointLight(0xffffff, 2.0, 1000);
+//     let light3 = new THREE.PointLight(0xffffff, 2.0, 1000);
+//     let light4 = new THREE.PointLight(0xffffff, 1.0, 1000);
+//   // устанавливаем координаты света
+//     light.position.set(800, -350, 450);
+//     light1.position.set(-100, -100, 350);
+//     light2.position.set(200, -750, -50);
+//     light3.position.set(1100, -450, -50);
+//     light4.position.set(1000, -50, 650);
+    // this.scene.add(light, light1, light2, light3, light4);
 
   /*let pointLightHelper = new THREE.PointLightHelper( light, 100 ); // вспомогательные вектора света
     let pointLightHelper1 = new THREE.PointLightHelper( light1, 100 );
@@ -39,4 +33,5 @@ let light = new THREE.PointLight(0xffffff, 1.0, 1000);
     scene.add(pointLightHelper2);
     scene.add(pointLightHelper3);
     scene.add(pointLightHelper4);*/
-  scene.add(light, light1, light2, light3, light4);
+
+};
--- public/js/scene.js
+++ public/js/scene.js
@@ -8,8 +8,13 @@ function scene() {
     scene.position.x = -380; // центруем сцену
     let camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 10000);// создание камеры
     camera.position.set(0, -1600, 700);camera.name = 'camera'; // устанавливаем позицию камеры
+    scene.add (camera);
     let renderer = new THREE.WebGLRenderer({ canvas: canvas });// создаем поле отображения
     renderer.setSize( window.innerWidth, window.innerHeight-4); // задаем размер
     document.body.appendChild( renderer.domElement );
+light();
     // scene.add (board);
+console.log(scene);
+    /*scene.add (light);*/
+
 }
